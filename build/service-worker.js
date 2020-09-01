744885873dd710f20c1049d446f83ff6742323d0
k-chew <kailey.chew@uwaterloo.ca>
2020-09-01 14:44:11 -0700

    react-create-app on Heroku

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.31183b2eb7b4a461113496dfbbde51a0.js"
+  "/precache-manifest.3eb7d548def96a35b36d9f3817d68cf0.js"
 );
 
 self.addEventListener('message', (event) => {
