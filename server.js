0bfbefa729cb7191a9c688371bbbfbbeb002e2b5
k-chew <kailey.chew@uwaterloo.ca>
2020-09-04 12:32:50 -0700

    Enable production path

--- server.js
+++ server.js
@@ -10,11 +10,14 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(cors());
 app.use(express.static('public'));
 
-/* production build code
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});*/
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
