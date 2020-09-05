04beeb35208c8f68d15cae763241c2924349cd3f
k-chew <kailey.chew@uwaterloo.ca>
2020-09-04 17:29:28 -0700

    build first

---
+++ client/build/precache-manifest.ccee26db850d237a90d7f09ac16497a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e87ce7726250826a6b9801ae699f8685",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f984a9637a070e7e456a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a2febebc6ef80e16ebf",
+    "url": "/static/js/2.3ce8bc22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3ce8bc22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f984a9637a070e7e456a",
+    "url": "/static/js/main.9f5ae6ae.chunk.js"
+  },
+  {
+    "revision": "b693d3b67e6a799813ba",
+    "url": "/static/js/runtime-main.0cbdd9ac.js"
+  },
+  {
+    "revision": "a341e8bb105a747c2e97537f2aec8b0b",
+    "url": "/static/media/logo.a341e8bb.svg"
+  }
+]);
